10d8fb835fd60d5b624cddd2a1209dc3f2b4934b
Samuel Cochran <sj26@sj26.com>
2015-01-13 10:28:34 +1100

    Bump git-buildkite to v0.3

--- git-buildkite.rb
+++ git-buildkite.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class GitBuildkite < Formula
   homepage "https://github.com/sj26/git-buildkite"
-  url "https://github.com/sj26/git-buildkite/archive/v0.2.tar.gz"
+  url "https://github.com/sj26/git-buildkite/archive/v0.3.tar.gz"
   head "https://github.com/sj26/git-buildkite.git"
 
   def install
