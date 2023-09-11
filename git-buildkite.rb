8ae72e50aa0b07945ca53b02e125a861b871be4a
Samuel Cochran <sj26@sj26.com>
2023-09-11 15:03:35 +1000

    git-buildkite: v0.6.0

--- git-buildkite.rb
+++ git-buildkite.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class GitBuildkite < Formula
   homepage "https://github.com/sj26/git-buildkite"
-  url "https://github.com/sj26/git-buildkite/archive/v0.5.tar.gz"
+  url "https://github.com/sj26/git-buildkite/archive/v0.6.0.tar.gz"
   head "https://github.com/sj26/git-buildkite.git"
 
   def install
